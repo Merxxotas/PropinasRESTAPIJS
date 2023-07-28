f62f09710b15d573a9057370200d319c465c7554
Merxxotas <brayan6590@gmail.com>
2023-07-28 13:02:15 -0500

    Subiendo a Github...

---
+++ js/app.js
@@ -0,0 +1,104 @@
+let cliente = {
+  mesa: "",
+  hora: "",
+  pedido: [],
+};
+
+const categorias = {
+  1: "Comida",
+  2: "Bebidas",
+  3: "Postres",
+};
+
+const btnGuardarCliente = document.querySelector("#guardar-cliente");
+btnGuardarCliente.addEventListener("click", guardarCliente);
+
+function guardarCliente() {
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
+
+  // Revisar si hay campos vacios
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
+  if (camposVacios) {
+    // console.log('Si hay campos vacios');
+
+    // Mostrar mensaje de error en el modal-body
+    const existeAlerta = document.querySelector(".invalid-feedback");
+    if (!existeAlerta) {
+      const alerta = document.createElement("DIV");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
+      document.querySelector(".modal-body form").appendChild(alerta);
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
+    }
+    return;
+  }
+  //   console.log("Todos los campos están llenos");
+  //   console.log(cliente);
+
+  cliente = { ...cliente, mesa, hora };
+
+  //ocultando el modal
+  const modalFormulario = document.querySelector("#formulario");
+  const modal = bootstrap.Modal.getInstance(modalFormulario);
+  modal.hide();
+
+  mostrarSecciones();
+  //obtener platillos de la API de JSON-Server
+  obtenerPlatillos();
+}
+
+function mostrarSecciones() {
+  const seccionesOcultas = document.querySelectorAll(".d-none");
+  seccionesOcultas.forEach((seccion) => seccion.classList.remove("d-none"));
+}
+
+function obtenerPlatillos() {
+  const url = "http://localhost:4000/platillos";
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => mostrarPlatillos(resultado))
+    .catch((error) => console.log(error));
+}
+
+function mostrarPlatillos(platillos) {
+  const contenido = document.querySelector("#platillos .contenido");
+  platillos.forEach((platillo) => {
+    const row = document.createElement("DIV");
+    row.classList.add("row", "py-3", "border-top");
+    const nombre = document.createElement("DIV");
+    nombre.classList.add("col-md-4", "py-3");
+    nombre.textContent = platillo.nombre;
+    const precio = document.createElement("DIV");
+    precio.classList.add("col-md-3", "py-3", "fw-bold");
+    precio.textContent = `$${platillo.precio}`;
+    const categoria = document.createElement("DIV");
+    categoria.classList.add("col-md-3", "py-3");
+    categoria.textContent = categorias[platillo.categoria];
+    const inputCantidad = document.createElement("INPUT");
+    inputCantidad.type = "number";
+    inputCantidad.min = 0;
+    inputCantidad.value = 0;
+    inputCantidad.id = `producto-${platillo.id}`;
+    inputCantidad.classList.add("form-control");
+    //Funcion para detectar la cantidad del platillo que se está agregando
+    inputCantidad.onchange = function () {
+      const cantidad = parseInt(inputCantidad.value);
+      agregarPlatillo({ ...platillo, cantidad });
+    };
+    const agregar = document.createElement("DIV");
+    agregar.classList.add("col-md-2", "py-3");
+    agregar.appendChild(inputCantidad);
+    row.appendChild(nombre);
+    row.appendChild(precio);
+    row.appendChild(categoria);
+    row.appendChild(agregar);
+    contenido.appendChild(row);
+  });
+}
+
+function agregarPlatillo(producto) {
+  console.log(producto);
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Implementación de un proyecto de Propinas a partir de unos platillos especificos, haciendo uso de los conceptos de REST API en JS.
+
+En este proyecto que hace parte de un curso sobre JS moderno, he creado una implementación de un sistema de propinas que se basa a partir de ciertos platillos en especifico que se consume desde una REST API. Utilizando JavaScript como lenguaje de programación. <u><strong>El objetivo principal es aplicar el concepto de como implementar las API en JS.</strong></u>
+
+## Tecnologías
+
+- JavaScript
+- HTML5/CSS3, Boostrap
+- [Aquí la DEMO]()
+
+## Aprendiendo JavaScript
+
+- Brayan Marín Guirales
+- [Más información del curso de JavaScript en cuestión](https://www.udemy.com/course/javascript-moderno-guia-definitiva-construye-10-proyectos/)
+- [Más información de mi persona](https://www.linkedin.com/in/brayan-marin-guirales/)
+
+## Julio de 2023
